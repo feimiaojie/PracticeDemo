4e16b98c08381bd8f02a2807fb20ed259d78af4e
miaojiefei <1074759572@qq.com>
2017-06-20 01:38:43 +0800

    MVC demo

---
+++ MvcViewApp/MvcViewApp/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcViewApp.Controllers
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewData["One"] = "view data";
+            ViewBag.Two = "view bag";
+            var _user = new User() { Name="lixuhuan"};
+            TempData["Four"] = "template data";
+            return View(_user);
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
