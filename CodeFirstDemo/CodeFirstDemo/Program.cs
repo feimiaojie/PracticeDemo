08e5109bf8fe678b5d2423a2212669b1e3438386
miaojiefei <1074759572@qq.com>
2017-08-06 00:06:03 +0800

    code first

---
+++ CodeFirstDemo/CodeFirstDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var ctx = new SchoolContext())
+                {
+                    Student s = new Student {StudentName="new student" };
+                    ctx.Students.Add(s);
+                    ctx.SaveChanges();
+                }
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+
+            }
+        }
+    }
+}
---
+++ CodeFirstDemo/CodeFirstDemo/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+    public class Student
+    {
+        public Student()
+        {
+
+        }
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+
+        public Standard CurrentStandard { get; set; }
+       // public Standard PreviousStandard { get; set; }
+    }
+
+    public class Standard
+    {
+        public Standard()
+        {
+
+        }
+        public int StandardId { get; set; }
+        public string StandardName { get; set; }
+
+        public ICollection<Student> CurrentStudents { get; set; }
+        //public ICollection<Student> PreviousStudents { get; set; }
+
+    }
+}
