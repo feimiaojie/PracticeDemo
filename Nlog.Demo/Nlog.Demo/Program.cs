3f68f91362a4bbcfc8cb8c4bb685ed3a231136d6
miaojiefei <1074759572@qq.com>
2018-01-16 23:52:00 +0800

    Add Nlog

---
+++ Nlog.Demo/Nlog.Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlog.Demo
+{
+    class Program
+    {
+        private static NLog.Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Trace("Trace Message");
+            logger.Debug("Debug Message");
+            logger.Info("Info Message");
+            logger.Error("Error Message");
+            logger.Fatal("Fatal Message");
+            //Logger.Default.Debug("123");
+            //Debug.Print("222222222222");
+        }
+    }
+}
