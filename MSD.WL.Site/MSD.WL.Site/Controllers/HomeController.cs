004ada4edeb9e199f4f603a2878ab108879f90b5
miaojiefei <1074759572@qq.com>
2018-04-04 23:20:02 +0800

    财务对账系统

---
+++ MSD.WL.Site/MSD.WL.Site/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSD.WL.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "财务模块";
+            return View();
+        }
+
+        public ActionResult Top()
+        {
+            ViewBag.UserName = "超级管理员";
+            ViewBag.AvailableBalance = "8888.00";
+            return View();
+        }
+
+        public ActionResult Left()
+        {
+
+            return View();
+        }
+
+        public ActionResult Right()
+        {
+            return View();
+        }
+    }
+}
